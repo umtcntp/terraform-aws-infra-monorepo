3a7e0b643f08fab6aaed15ae6e489c10107cfd61
umtcntp <umutcantop@mail.com>
2026-05-10 14:31:01 +0300

    user-data

---
+++ tf-user-data/main.tf
@@ -0,0 +1,92 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "ec2_example" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.main.id]
+  key_name               = aws_key_pair.deployer.key_name
+  user_data              = <<-EOF
+            #!/bin/bash
+            echo "Hello, World!" >hello.txt
+            EOF
+}
+
+resource "aws_security_group" "main" {
+  name        = "ec2-example-sg"
+  description = "Allow SSH and HTTPS"
+
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 443
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 443
+    }
+  ]
+}
+
+terraform {
+  backend "s3" {
+    bucket       = "umut-terraform-bucket"
+    key          = "ec2-demo/terraform.tfstate"
+    region       = "eu-central-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "aws-key-pair"
+  public_key = file("../ec2-instance-keys/aws-key-pair.pub")
+}
+
+output "fetched_info_from_aws" {
+  value = format("%s%s", "ssh -i /Users/rahulwagh/.ssh/aws_ec2_terraform ubuntu@", aws_instance.ec2_example.public_dns)
+}
